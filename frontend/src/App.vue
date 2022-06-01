7931720e9b7c18bcdcd8d35965ac2b9db95d38cd
Yash Vijay <yvvijay121@gmail.com>
2022-06-01 17:38:16 -0500

    no longer using a linter, decided to restart the frontend entirely

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,10 +3,10 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view/>
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,14 +17,14 @@
 
 nav {
   padding: 30px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
