d22933aa9afcbd08895bc7cc10e73080fe600629
Yash Vijay <yvvijay121@gmail.com>
2022-06-01 22:05:40 -0500

    frontend is starting to work, created homepage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar is-transparent">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"
+        ><img
+          src="https://bulma.io/images/bulma-logo.png"
+          alt="Bulma: a modern CSS framework based on Flexbox"
+          width="112"
+          height="28"
+      /></router-link>
+    </div>
+
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+      <div class="navbar-end">
+        <router-link to="/about" class="navbar-item">About</router-link>
+      </div>
+    </div>
   </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div
+    class="
+      hero
+      columns
+      is-mobile is-vcentered is-centered is-fullheight-with-navbar
+    "
+  >
+    <div class="column is-narrow">
+      <div class="box has-text-centered">
+        <h3 class="title is-3 m-5">CITATURE</h3>
+        <div class="field">
+          <div class="control">
+            <input
+              class="input"
+              type="text"
+              :placeholder="'Find an article by ' + typeOfID"
+              v-model="articleID"
+              @keyup.enter="searchRedirect()"
+            />
+          </div>
+        </div>
+        <div class="field has-addons">
+          <div class="control is-expanded">
+            <div class="select is-fullwidth">
+              <select v-model="typeOfID">
+                <option disabled value="">Please select one</option>
+                <option>DOI</option>
+                <option>Pubmed</option>
+                <option>PMC</option>
+              </select>
+            </div>
+          </div>
+          <div class="control">
+            <button class="button is-primary" @click="searchRedirect()">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+      <article
+        class="message is-danger box p-0"
+        :style="{ visibility: validationerror === '' ? 'hidden' : 'visible' }"
+      >
+        <div class="message-body">
+          {{ validationerror }}
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+const regex = /(10\.[0-9a-zA-Z]+\/(?:(?!["&\'])\S)+)\b/;
+const articleID = ref('');
+const typeOfID = ref('DOI');
+const router = useRouter();
+const validationerror = ref('');
+
+function searchRedirect() {
+  let cleanedText = articleID.value.match(regex);
+  if (cleanedText === null) {
+    validationerror.value = 'Please enter a valid publication identifier.';
+  } else {
+    router.push('/info/' + articleID.value);
+  }
+}
 </script>
