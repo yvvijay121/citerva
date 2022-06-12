9e7ab30c006eebbd69c61ceace362198ae9e7b75
Yash Vijay <yvvijay121@gmail.com>
2022-06-11 19:00:49 -0500

    debugging changes, having issues with backend

--- backend/src/main/java/io/github/yvvijay121/Main.java
+++ backend/src/main/java/io/github/yvvijay121/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8080);
+        app.before(ctx -> System.out.println("Request: " + ctx.req.getMethod() + " " + ctx.req.getRequestURI()));
         app.get("/", ctx -> ctx.status(400).result("Please do not use this endpoint directly."));
         app.get("/doi/<id>", DOIController::retrieve);
         app.get("/graph/<id>", CitationMapController::retrieveGraph);
