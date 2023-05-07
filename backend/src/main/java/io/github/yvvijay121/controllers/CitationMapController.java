66fd148a458712500c9c39986fb5d1d7114a3298
Yash Vijay <yvvijay121@gmail.com>
2023-05-07 00:37:49 -0500

    this change makes the program not DDOS the API

--- backend/src/main/java/io/github/yvvijay121/controllers/CitationMapController.java
+++ backend/src/main/java/io/github/yvvijay121/controllers/CitationMapController.java
@@ -15,42 +15,32 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static io.github.yvvijay121.Main.EMAIL;
 
 public class CitationMapController {
     public static void retrieveGraph(Context ctx) throws URISyntaxException, IOException, InterruptedException {
+        // get the root object from the DOI
         String id = ctx.pathParam("id");
         HttpResponse<String> a = DOIController.getOpenAlexObject(id);
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode json = objectMapper.readTree(a.body());
+
+        // create the root vertex
         String root = json.get("id").asText().replace("https://openalex.org/", "");
         String rootTitle = json.get("title").asText();
         String rootDoi = json.get("doi").asText();
         Vertex rootVertex = new Vertex(root, rootTitle, rootDoi);
+
         // get the cited articles from the object, and convert them to Stream<Vertex>
         Graph graph = new Graph(rootVertex);
         JsonNode citedArticles = json.get("referenced_works");
         HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
-        objectMapper.convertValue(citedArticles, new TypeReference<List<String>>() {
-        }).stream().map(s -> s.replace("https://openalex.org/", "")).map(s -> {
-            try {
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(new URI("https://api.openalex.org/works/" + s))
-                        .header("User-Agent", "mailto:" + EMAIL)
-                        .GET()
-                        .build();
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                JsonNode jsonNode = objectMapper.readTree(response.body());
-                return new Vertex(s, jsonNode.get("title").asText(), jsonNode.get("doi").asText());
-            } catch (URISyntaxException | IOException | InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }).forEach(v -> {
-            graph.addVertex(v);
-            graph.addEdge(new Edge("edge-" + UUID.randomUUID(), root, v.getId()));
-        });
-        ctx.json(graph);
+
+        String citedArticlesString = objectMapper.convertValue(citedArticles, new TypeReference<List<String>>() {
+        }).stream().map(s -> s.replace("https://openalex.org/", "")).collect(Collectors.joining("|"));
+
+        ctx.json(citedArticlesString);
     }
 }
